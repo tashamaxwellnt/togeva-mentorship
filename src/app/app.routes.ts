cc1cdf29e3cd0bda2cbd9446e9b469615893e16f
Tasha Maxwell <tashantengkeh@gmail.com>
2025-04-03 18:34:21 +0100

    configured routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './routing/landing/landing.component';
+import { SignupComponent } from './routing/signup/signup.component';
+import { LoginComponent } from './routing/login/login.component';
+import { ErrorComponent } from './routing/error/error.component';
+import { ForgotPasswordComponent } from './routing/forgot-password/forgot-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'signup',
+        component: SignupComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'fp', 
+        component: ForgotPasswordComponent
+    },
+    {
+        path: '**',
+        component: ErrorComponent
+    }
+];
---
+++ src/app/routing/landing/landing.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './landing.component.html',
+  styles: ``
+})
+export class LandingComponent {
+
+}
